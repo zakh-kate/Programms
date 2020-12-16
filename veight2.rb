230393226762126d8b4863059ffcb7deb7ea98d6
zakh-kate <73604976+zakh-kate@users.noreply.github.com>
2020-12-16 20:36:07 +0700

    Update veight2.rb

--- veight2.rb
+++ veight2.rb
@@ -4,10 +4,10 @@ name = gets.chomp
 puts "Какой у тебя рост?"
 veigh = gets.chomp
 
-a=(veigh.to_i-110)*1.15
+a = (veigh.to_i - 110) * 1.15
 
-if a>0
-puts "#{name}, твой идеальный вес #{a} кг"
+if a > 0
+  puts "#{name}, твой идеальный вес #{a} кг"
 end
 
 unless puts "Твой вес уже оптимальный"
