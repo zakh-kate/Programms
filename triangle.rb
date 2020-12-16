a43adb50b084dc26036a981b26fc78e669d889dd
zakh-kate <73604976+zakh-kate@users.noreply.github.com>
2020-12-16 20:35:13 +0700

    Update triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -8,7 +8,7 @@ puts "Третья сторона треугольник равна..."
 c = gets.chomp
 
 if ((a <= 0) || (b <= 0) || (c <= 0))
-puts "треугольника не существует"
+  puts "треугольника не существует"
 end
 z1 = 0 
 z2 = 0 
@@ -16,37 +16,37 @@ z3 = 0
 z4 = 0 
 z5 = 0
 if (a == b) && (b == c) 
-puts "Треугольник равносторонний, но не прямоугольный"
-z1 = 1
+  puts "Треугольник равносторонний, но не прямоугольный"
+  z1 = 1
 end
 
 if ((a == b) && (b != c) )|| ((b == c) && (c != a))|| ((c == a) && (b != c)) 
-puts "Треугольник равнобедренный"
-z2 = 1
+  puts "Треугольник равнобедренный"
+  z2 = 1
 end
 
 if (a > b) && (a > c)
-gipotenuza = (b.to_i ** 2 + c.to_i ** 2) ** 0.5
-if a == gipotenuza
-puts "Треугольник прямоугольный"
-z3 = 1
-end
+ gipotenuza = (b.to_i ** 2 + c.to_i ** 2) ** 0.5
+  if a == gipotenuza
+  puts "Треугольник прямоугольный"
+  z3 = 1
+  end
 end
 
 if (b > a) && (b > c)
-gipotenuza = (a.to_i ** 2 + c.to_i ** 2) ** 0.5
-if b == gipotenuza
-puts "Треугольник прямоугольный"
-z4 = 1
-end
+ gipotenuza = (a.to_i ** 2 + c.to_i ** 2) ** 0.5
+  if b == gipotenuza
+  puts "Треугольник прямоугольный"
+  z4 = 1
+  end
 end
 
 if (c > a) && (c > b)
-gipotenuza = (a.to_i ** 2 + b.to_i ** 2) ** 0.5
-if c == gipotenuza
-puts "Треугольник прямоугольный"
-z5 = 1
-end
+ gipotenuza = (a.to_i ** 2 + b.to_i ** 2) ** 0.5
+  if c == gipotenuza
+  puts "Треугольник прямоугольный"
+  z5 = 1
+  end
 end
 
 sum = z1 + z2 + z3 + z4 + z5
