0adac7481bdf32aa01830316ebf145132be013e6
zakh-kate <73604976+zakh-kate@users.noreply.github.com>
2020-12-16 20:27:46 +0700

    Update discriminant.rb

--- discriminant.rb
+++ discriminant.rb
@@ -10,7 +10,7 @@ c = gets.chomp
 D = b.to_i ** 2 - (4 * a.to_i * c.to_i)
 if D > 0
 x1 = (-b.to_i + D ** 0.5) / (2 * a.to_i)
-x2 = (-b.to_i - D ** 0.5)/( 2 * a.to_i)
+x2 = (-b.to_i - D ** 0.5) / ( 2 * a.to_i)
 puts "Дискриминант равен #{D},первый корень #{x1}, второй корень #{x2}"
 end
 
